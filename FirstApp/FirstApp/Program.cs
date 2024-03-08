0ddc3b14431127422177a50f745599f79dd15e27
s27485 <s27485@pjwstk.edu.pl>
2024-03-08 15:22:50 +0100

    Modifikacja 3

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -4,17 +4,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Siema to ja koszykarz.");
 Console.WriteLine("No witam witam!");
 
-static double GetAverage(int [] number)
-{
-    double result = 0.0;
-    for (int i = 0; i < number.Length; i++)
-    {
-        result += number[i];
-    }
-
-    return result / number.Length;
-}
-
-int[] number = {1, 2, 3, 4};
-double avg = GetAverage(number);
-Console.WriteLine(avg);
